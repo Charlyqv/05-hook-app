3af5dd790338cb0def821e2ea35b8060d6d9fa2f
Carlos Villaseñor Quintero <charlyqv98@gmail.com>
2023-06-23 23:01:43 -0600

    Formulario con custom Hook

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import { CounterApp } from './01-useState/CounterApp';
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
-import { SimpleForm } from './02-useEffect/SimpleForm';
+// import { SimpleForm } from './02-useEffect/SimpleForm';
+import { FormWhithCustomHook } from './02-useEffect/FormWithCustomHook';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <SimpleForm />
+    <FormWhithCustomHook />
   // </React.StrictMode>
 )
