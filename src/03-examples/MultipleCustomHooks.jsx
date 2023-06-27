41039fc8b8faac127eabda36f98ad07d5bebe1e5
Carlos <charlyqv98@gmail.com>
2023-06-27 08:52:02 -0600

    useFetch + useCounter

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -4,12 +4,36 @@ export const MultipleCustomHooks = () => {
 
   const { data, isLoading, hasError } = useFetch('https://pokeapi.co/api/v2/pokemon-form/4');
 
-  console.log({ data, isLoading, hasError });
-  
+  // console.log( data );
+
+  const { id, name } = !!data && data;
+
   return (
     <>
       <h1>BreakingBad Quotes</h1>
       <hr />
+
+      {
+        isLoading 
+          ? (
+              <div className="alert alert-info text-center">
+                Loading...
+              </div>
+
+          )
+          : (
+              <blockquote className="blockquote text-end">
+                <p className="mb-1">{ id }</p>
+                <footer className="blockquote-footer"> { name } </footer>
+              </blockquote>
+          )
+      }
+
+      <button className="btn btn-primary">
+        Next quote
+      </button>
+
+
     </>
   )
 }
